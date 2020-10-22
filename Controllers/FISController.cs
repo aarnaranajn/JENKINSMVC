fad666493d92cf0f6dd2a765e199a73dca5b3e64
ANOOP RANJAN <anooppriyaranjan@gmail.com>
2020-10-22 13:39:52 +0530

    FIS CONTROLLER

---
+++ Controllers/FISController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JENKINSMVC.Controllers
+{
+    public class FISController : Controller
+    {
+        // GET: FIS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
